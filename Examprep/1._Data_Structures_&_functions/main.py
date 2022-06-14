88b4b841ba8f7730c4bf512a99baf1ab9a430533
jona881a <jonaskunert@hotmail.com>
2022-06-14 07:10:06 +0200

    something

--- Examprep/1._Data_Structures_&_functions/main.py
+++ Examprep/1._Data_Structures_&_functions/main.py
@@ -1,11 +1,11 @@
-from dealership import Dealership
-from car import Car
-import datetime
+from car import Car;
+import datetime;
 
-dealership = Dealership()
+dealership = [
+  Car('Ford','Mustang',datetime.datetime(1969, 1, 1),'Musclecar', 800000), 
+  Car('Nissan','Skyline R34', datetime.datetime(2000, 1, 1), 'Tuner', 450000), 
+  Car('Ford','GT',datetime.datetime(2010,2,10),'Supercar',3200000), 
+  Car('Bugatti','Veyron 16.4',datetime.datetime(2012,10,10),'Hypercar',12000000)
+]
 
-dealership + Car('Ford','Mustang',datetime.datetime(1969, 1, 1),'Musclecar', 800000)
-
-dealership + Car('Nissan','Skyline R34', datetime.datetime(2000, 1, 1), 'Tuner', 450000)
-
-str(dealership)
+print(dealership)
